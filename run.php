f08d6a18fc00ed5023895855b6099bc30712ec64
Ayesh Karunaratne <ayesh@aye.sh>
2021-01-18 22:42:03 +0700

    Add support to take values from

--- run.php
+++ run.php
@@ -5,9 +5,9 @@ use Ayesh\GeoIPTreeBuilder\Builder;
 require __DIR__ . '/vendor/autoload.php';
 
 $builder = new Builder(
-    dataPath: 'input/GeoLite2-Country-Blocks-IPv4.csv',
-    locationListPath: 'input/GeoLite2-Country-Locations-en.csv',
-    target_dir: __DIR__ . '/../Geo-IP-Database/data'
+    dataPath: $argv[1] ?? 'input/GeoLite2-Country-Blocks-IPv4.csv',
+    locationListPath: $argv[2] ?? 'input/GeoLite2-Country-Locations-en.csv',
+    target_dir: $argv[3] ?? __DIR__ . '/../Geo-IP-Database/data'
 );
 
 $builder->run();
